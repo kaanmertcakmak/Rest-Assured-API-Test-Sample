2a55cac9180ab18b061a6e9fe16d3cfa26a0301e
kaan.cakmak <Kmchkn1234>
2020-07-06 03:33:37 +0300

    first

---
+++ src/test/java/utils/APITestCase.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class APITestCase {
+    // I have created mock API in order to check some of my tests
+    public static final String API_ROOT = "https://5f01b5829e41230016d4277d.mockapi.io/api";
+}
